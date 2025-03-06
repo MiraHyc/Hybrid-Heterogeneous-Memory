bce373f606b58ad177538c1d2df0114e05f50edd
Mira <1307065365@qq.com>
2025-03-06 03:43:46 +0000

    链接-lnuma

--- include/Common.h
+++ include/Common.h
@@ -100,12 +100,11 @@ constexpr int64_t kPerThreadRdmaBuf  = rdmaBufferSize * define::GB / MAX_APP_THR
 constexpr int64_t kPerCoroRdmaBuf    = kPerThreadRdmaBuf / MAX_CORO_NUM;
 
 // Cache (MB)
-constexpr int kIndexCacheSize = 600;
+constexpr int kIndexCacheSize = 25;
 
 // KV
 constexpr uint32_t keyLen = 8;
 constexpr uint32_t simulatedValLen = 8;
->>>>>>> Stashed changes
 constexpr uint32_t allocAlignLeafSize = ROUND_UP(keyLen + simulatedValLen + 8 + 2, ALLOC_ALLIGN_BIT);
 
 // Tree
